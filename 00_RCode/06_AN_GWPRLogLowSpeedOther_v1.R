a59931742b911464719d5774c163ff2f4a3b14ad
Chao Li <chaoli0394@gmail.com>
2022-06-01 12:36:30 +0900

    A: run it on the desktop; revise 06_AN; done

--- 00_RCode/06_AN_GWPRLogLowSpeedOther_v1.R
+++ 00_RCode/06_AN_GWPRLogLowSpeedOther_v1.R
@@ -10,6 +10,9 @@ library(tmap)
 library(sp)
 library(doParallel)
 library(foreach)
+library(foreach)
+library(doSNOW)
+library(parallel)
 
 load("04_Data/02_panelLowSpeedDensityDataset.RData")
 load("04_Data/03_dayTempRasterDataset.ag.RData")
@@ -176,3 +179,41 @@ GWPR.FEM.CV.F.result <- GWPR(formula = formula, data = dataset_used.Tokyo%>%rena
 GWPR.FEM.CV.F.result$SDF@data %>% View()
 summary(GWPR.FEM.CV.F.result$SDF@data$temp_TVa %>% as.numeric())
 save(GWPR.FEM.CV.F.result, file = "03_Results/GWPR_FEM_CV_F_result_1.15.Rdata")
+
+
+##### based on the SP_06_AN, We add this code to run  the code on the desktop
+source("00_RCode/SP_07_AF_GWPRBandwidthStepSelection_v1.R")
+#points_mesh.in.Tokyo <- read.csv("04_Data/SP_00_points_mesh.test.RData")
+#dataset_used.Tokyo <- read.csv("04_Data/SP_00_dataset_used.test.RData")
+points_mesh.in.Tokyo <- read.csv("04_Data/SP_00_points_mesh.in.Tokyo.RData")
+dataset_used.Tokyo <- read.csv("04_Data/SP_00_dataset_used.Tokyo.RData")
+proj <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0" 
+
+xy <- points_mesh.in.Tokyo[,c(2,3)] ### remember here is csv, they have a index colunm
+points_mesh.in.Tokyo <- SpatialPointsDataFrame(coords = xy, data = points_mesh.in.Tokyo,
+                                               proj4string = CRS(proj))
+message("data done")
+
+#formula <- lowSpeedDensity ~ temp +  NDVI + prevalance + emergence
+formula <- lowSpeedDensity ~   temp +  NDVI + ter_pressure +  precipitation +  
+  UVAerosolIndex + PBLH + prevalance + emergence
+
+dataset_used.Tokyo <- dataset_used.Tokyo %>% 
+  dplyr::select("GridID", "time", all.vars(formula)) %>% na.omit()
+
+### search log movement bandwidth for 0.02 to 0.50, step is 0.005 
+not_get_result <- T
+if(not_get_result){
+  message(formula)
+  #formula <- lowSpeedDensity_num ~ temp +  NDVI + prevalance + emergence
+  GWPR.FEM.bandwidth <- # this is about fixed bandwidth
+    bw.GWPR.step.selection(formula = formula, data = dataset_used.Tokyo, index = c("GridID", "time"),
+                           SDF = points_mesh.in.Tokyo, adaptive = F, p = 2, bigdata = F,
+                           upperratio = 0.10, effect = "individual", model = "within", approach = "CV",
+                           kernel = "bisquare", doParallel = T, cluster.number = 15, gradientIncrecement = T,
+                           GI.step = 0.005, GI.upper = 0.5, GI.lower = 0.015,  
+                           address.output  =  "03_Results/15_laptop_F_8var_0015_05_0005.RData")
+  GWPR.FEM.bandwidth.step.list <- GWPR.FEM.bandwidth
+  save(GWPR.FEM.bandwidth.step.list, file = "03_Results/GWPR_BW_setp_list.Tokyo.log.var8.0015.05.0005.Rdata")
+  not_get_result <- F
+}
