f292b735feef530551d82cc33ea4bf4e75491bc6
MichaelChaoLi-cpu <chaoli0394@gmail.com>
2023-05-10 15:19:52 +0900

    A: revise the code

--- 02_PyCode/M09_AN_TuneHyperXgb_v1.py
+++ 02_PyCode/M09_AN_TuneHyperXgb_v1.py
@@ -48,7 +48,43 @@ def tuningHyperLr(X, y, n_estimators, learning_rate_list):
             best_parameter = interest
     return best_score, best_parameter
 
+def tuningHyperMaxDepth(X, y, n_estimators, learning_rate,
+                        tuning_list):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1,
+                                                        random_state=42)
+    best_score = 0
+    best_parameter = 0
+    for interest in tuning_list:
+        xgb_regressor = xgb.XGBRegressor(n_estimators = n_estimators, n_jobs=-1,
+                                         learning_rate = learning_rate, 
+                                         max_depth = interest)
+        xgb_regressor.fit(X_train, y_train)
+        y_pred = xgb_regressor.predict(X_test)
+        accuracy = r2_score(y_test, y_pred)
+        print(f"Parameter: {interest}; Accuracy: {accuracy*100:.2f}%")
+        if accuracy > best_score:
+            best_score = accuracy
+            best_parameter = interest
+    return best_score, best_parameter
 
+def tuningHyperGamma(X, y, n_estimators, learning_rate, max_depth,
+                     tuning_list):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1,
+                                                        random_state=42)
+    best_score = 0
+    best_parameter = 0
+    for interest in tuning_list:
+        xgb_regressor = xgb.XGBRegressor(n_estimators = n_estimators, n_jobs=-1,
+                                         learning_rate = learning_rate, 
+                                         max_depth = max_depth, gamma = interest)
+        xgb_regressor.fit(X_train, y_train)
+        y_pred = xgb_regressor.predict(X_test)
+        accuracy = r2_score(y_test, y_pred)
+        print(f"Parameter: {interest}; Accuracy: {accuracy*100:.2f}%")
+        if accuracy > best_score:
+            best_score = accuracy
+            best_parameter = interest
+    return best_score, best_parameter
 
 if __name__ == '__main__':
     REPO_LOCATION = runLocallyOrRemotely('y')
@@ -57,8 +93,13 @@ if __name__ == '__main__':
     best_score, best_n_estimators = tuningHyperNestimator(X, y, 
                                                           [100, 200, 300, 400, 500,
                                                            600, 700, 800, 900, 1000])
+    ### n_estimators = 500
     best_score, best_lr = tuningHyperLr(X, y, 500, 
-                                        [0.01, 0.05, 0.1, 0.2, 0.5])
-    best_score, best_lr = tuningHyperLr(X, y, 500, 
-                                        [0.6, 0.7, 0.8, 0.9])
-    
+                                        [0.01, 0.05, 0.1, 0.2, 0.5, 0.6, 0.8])
+    ### lr = 0.5
+    best_score, best_maxdepth = tuningHyperMaxDepth(X, y, 500, 0.5,
+                                                    [3, 4, 5, 6, 7, 8, 9, 10,
+                                                     11, 12, 13, 14, 15])
+    ### max_depth = 9
+    best_score, best_gamma = tuningHyperGamma(X, y, 500, 0.5, 9, 
+                                                 [0, 1, 2, 3, 4, 5])
