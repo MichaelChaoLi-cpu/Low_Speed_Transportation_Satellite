65806afac432ab3729b98a8c4877b4ad94b738ea
MichaelChaoLi-cpu <chaoli0394@gmail.com>
2023-05-15 17:34:20 +0900

    C: rf is best but slow, so choose XGB

--- 02_PyCode/08_AN_CompareMachineLearningModel_v1.py
+++ 02_PyCode/08_AN_CompareMachineLearningModel_v1.py
@@ -26,8 +26,8 @@ def runLocallyOrRemotely(Locally_Or_Remotely):
         repo_location = "/home/usr6/q70176a/DP11/"
     elif locally_or_remotely == 'wsl':
         repo_location = "/mnt/d/OneDrive - Kyushu University/11_Article/03_RStudio/"
-    elif  locally_or_remotely == 'linux':
-        repo_location = "/mnt/d/OneDrive - Kyushu University/11_Article/03_RStudio/"
+    elif  locally_or_remotely == 'mac':
+        repo_location = "/Users/lichao/Library/CloudStorage/OneDrive-KyushuUniversity/11_Article/03_RStudio/"
     return repo_location
 
 def getXandY():
@@ -66,6 +66,13 @@ def TestModel(X, y):
     adaaccuracy = r2_score(y_test, y_pred)
     print(f"ada Accuracy: {adaaccuracy:.4f}")
 
+    gbmodel = GradientBoostingRegressor(
+        n_estimators=100, learning_rate=0.1, 
+        max_depth=3, random_state=42)   
+    gbmodel.fit(X_train, y_train)
+    y_pred = gbmodel.predict(X_test)
+    gbaccuracy = r2_score(y_test, y_pred)
+    print(f"GB Accuracy: {gbaccuracy:.4f}")
     
     olsmodel = LinearRegression()
     olsmodel.fit(X_train, y_train)
@@ -74,79 +81,12 @@ def TestModel(X, y):
     print(f"ols Accuracy: {olsaccuracy:.4f}")
     return xgaccuracy, rfaccuracy, adaaccuracy, olsaccuracy
 
-def testRfModel(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, 
-                                                        test_size=0.1, 
-                                                        random_state=42) 
-    
-    rfmodel = RandomForestRegressor(n_estimators=100, random_state=42, n_jobs=9)
-    rfmodel.fit(X_train, y_train)
-    y_pred = rfmodel.predict(X_test)
-    rfaccuracy = r2_score(y_test, y_pred)
-    print(f"rf 100 trees Accuracy: {rfaccuracy:.4f}")
-    
-    rfmodel = RandomForestRegressor(n_estimators=500, random_state=42, n_jobs=9)
-    rfmodel.fit(X_train, y_train)
-    y_pred = rfmodel.predict(X_test)
-    rfaccuracy = r2_score(y_test, y_pred)
-    print(f"rf 500 trees Accuracy: {rfaccuracy:.4f}")
-    
-    rfmodel = RandomForestRegressor(n_estimators=1000, random_state=42, n_jobs=9)
-    rfmodel.fit(X_train, y_train)
-    y_pred = rfmodel.predict(X_test)
-    rfaccuracy = r2_score(y_test, y_pred)
-    print(f"rf 1000 trees Accuracy: {rfaccuracy:.4f}")
-    
-    return None
 
-def testXgbModel(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, 
-                                                        test_size=0.1, 
-                                                        random_state=42) 
-    
-    xgmodel = xgb.XGBRegressor(n_estimators=100, learning_rate=0.1, max_depth=10,
-                               seed=42, n_jobs=-1) 
-    xgmodel.fit(X_train, y_train)
-    y_pred = xgmodel.predict(X_test)
-    xgaccuracy = r2_score(y_test, y_pred)
-    print(f"100 xg Accuracy: {xgaccuracy:.4f}")
-    
-    xgmodel = xgb.XGBRegressor(n_estimators=200, learning_rate=0.1, max_depth=10,
-                               seed=42, n_jobs=-1) 
-    xgmodel.fit(X_train, y_train)
-    y_pred = xgmodel.predict(X_test)
-    xgaccuracy = r2_score(y_test, y_pred)
-    print(f"200 xg Accuracy: {xgaccuracy:.4f}")
-    
-    xgmodel = xgb.XGBRegressor(n_estimators=300, learning_rate=0.1, max_depth=10,
-                               seed=42, n_jobs=-1) 
-    xgmodel.fit(X_train, y_train)
-    y_pred = xgmodel.predict(X_test)
-    xgaccuracy = r2_score(y_test, y_pred)
-    print(f"300 xg Accuracy: {xgaccuracy:.4f}")
-    
-    xgmodel = xgb.XGBRegressor(n_estimators=400, learning_rate=0.1, max_depth=10,
-                               seed=42, n_jobs=-1) 
-    xgmodel.fit(X_train, y_train)
-    y_pred = xgmodel.predict(X_test)
-    xgaccuracy = r2_score(y_test, y_pred)
-    print(f"400 xg Accuracy: {xgaccuracy:.4f}")
-    
-    xgmodel = xgb.XGBRegressor(n_estimators=500, learning_rate=0.1, max_depth=10,
-                               seed=42, n_jobs=-1) 
-    xgmodel.fit(X_train, y_train)
-    y_pred = xgmodel.predict(X_test)
-    xgaccuracy = r2_score(y_test, y_pred)
-    print(f"500 xg Accuracy: {xgaccuracy:.4f}")
-    
-    return None
-    
 
-REPO_LOCATION = runLocallyOrRemotely('y')
+REPO_LOCATION = runLocallyOrRemotely('mac')
 
 df, X, y = getXandY()
 
 TestModel(X, y)
-#testRfModel(X, y)
-testXgbModel(X, y)
+
 
