974a77e4c76f07b52c257b03a99af8f495d83c70
MichaelChaoLi-cpu <chaoli0394@gmail.com>
2023-05-17 21:46:21 +0900

    B: try to gwpr

--- 00_RCode/SP_11_AN_GwprConnectionXandShap_v1.R
+++ 00_RCode/SP_11_AN_GwprConnectionXandShap_v1.R
@@ -10,28 +10,27 @@ library(plm)
 library(sp)
 
 proj <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
-dataset_Xshap <- read.csv('03_Results/mergedXSHAP.csv')
+dataset_Xshap <- read.csv('/home/usr6/q70176a/DP11/mergedXSHAP.csv')
 
 #### build sp dataset
 point_dataset <- dataset_Xshap[,c(1,18,19)] %>% distinct()
 xy <- point_dataset[,c(2,3)]
 points_mesh <- SpatialPointsDataFrame(coords = xy, data = point_dataset,
                                       proj4string = CRS(proj))
-points_mesh@data <- points_mesh@data %>% dplyr::select(GridID)
 
-correlation_table <- cor(dataset_Xshap)
-### Temperature 
 formula <- Temperature_shap ~ Temperature
 
 GWPR.FEM.bandwidth.Temperature <- # this is about fixed bandwidth
   bw.GWPR(formula = formula, data = dataset_Xshap, index = c("GridID", "time"),
           SDF = points_mesh, adaptive = F, p = 2, bigdata = F,
           upperratio = 0.10, effect = "individual", model = "within", approach = "CV",
-          kernel = "bisquare",doParallel = T, cluster.number = 8, gradientIncrement = T,
+          kernel = "bisquare",doParallel = T, cluster.number = 30, gradientIncrement = T,
           GI.step = 0.0025, GI.upper = 0.1, GI.lower = 0.0025)
 saveRDS(GWPR.FEM.bandwidth.Temperature, '03_Results/GWPR.FEM.bandwidth.Temperature.rds')
-plot(GWPR.FEM.bandwidth.Temperature[,1], GWPR.FEM.bandwidth.Temperature[,2])
+#plot(GWPR.FEM.bandwidth.Temperature[,1], GWPR.FEM.bandwidth.Temperature[,2])
 
+
+points_mesh@data <- points_mesh@data %>% dplyr::select(GridID)
 GWPR.FEM.CV.F.result <- GWPR(formula = formula, data = dataset_Xshap, index = c("GridID", "time"),
                              SDF = points_mesh, bw = 0.015, adaptive = F,
                              p = 2, effect = "individual", kernel = "bisquare", longlat = F, 
@@ -49,20 +48,3 @@ summary(rem)
 pFtest(fem, ols)
 phtest(fem, rem)
 plmtest(ols, type = "bp")
-
-###
-formula <- NTL_shap ~ NTL
-
-GWPR.FEM.bandwidth.NTL <- # this is about fixed bandwidth
-  bw.GWPR(formula = formula, data = dataset_Xshap, index = c("GridID", "time"),
-          SDF = points_mesh, adaptive = F, p = 2, bigdata = F,
-          upperratio = 0.10, effect = "individual", model = "within", approach = "CV",
-          kernel = "bisquare",doParallel = T, cluster.number = 8, gradientIncrement = T,
-          GI.step = 0.0025, GI.upper = 0.1, GI.lower = 0.0025)
-saveRDS(GWPR.FEM.bandwidth.NTL, '03_Results/GWPR.FEM.bandwidth.NTL.rds')
-plot(GWPR.FEM.bandwidth.NTL[,1], GWPR.FEM.bandwidth.NTL[,2])
-
-GWPR.FEM.CV.F.result <- GWPR(formula = formula, data = dataset_Xshap, index = c("GridID", "time"),
-                             SDF = points_mesh, bw = 0.015, adaptive = F,
-                             p = 2, effect = "individual", kernel = "bisquare", longlat = F, 
-                             model = "pooling")
