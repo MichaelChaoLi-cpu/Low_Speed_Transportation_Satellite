23d271340773d24d2e6c5057f114412afcf63556
MichaelChaoLi-cpu <chaoli0394@gmail.com>
2023-06-05 15:17:57 +0900

    B: air pressure

--- 00_RCode/11_AN_GwprConnectionXandShap_v1.R
+++ 00_RCode/11_AN_GwprConnectionXandShap_v1.R
@@ -28,7 +28,7 @@ GWPR.FEM.bandwidth.Temperature <- # this is about fixed bandwidth
           SDF = points_mesh, adaptive = F, p = 2, bigdata = F,
           upperratio = 0.10, effect = "individual", model = "within", approach = "CV",
           kernel = "bisquare",doParallel = T, cluster.number = 8, gradientIncrement = T,
-          GI.step = 0.0025, GI.upper = 0.1, GI.lower = 0.0025)
+          GI.step = 0.0025, GI.upper = 0.061, GI.lower = 0.0025)
 saveRDS(GWPR.FEM.bandwidth.Temperature, '03_Results/GWPR.FEM.bandwidth.Temperature.rds')
 plot(GWPR.FEM.bandwidth.Temperature[,1], GWPR.FEM.bandwidth.Temperature[,2])
 
@@ -50,10 +50,25 @@ GWPR.FEM.bandwidth.NTL <- # this is about fixed bandwidth
 saveRDS(GWPR.FEM.bandwidth.NTL, '03_Results/GWPR.FEM.bandwidth.NTL.rds')
 plot(GWPR.FEM.bandwidth.NTL[,1], GWPR.FEM.bandwidth.NTL[,2])
 
-GWPR.FEM.CV.F.result <- GWPR(formula = formula, data = dataset_Xshap, index = c("GridID", "time"),
-                             SDF = points_mesh, bw = 0.015, adaptive = F,
-                             p = 2, effect = "individual", kernel = "bisquare", longlat = F, 
-                             model = "pooling")
+formula <- NDVI_shap ~ NDVI
 
+GWPR.FEM.bandwidth.NDVI <- # this is about fixed bandwidth
+  bw.GWPR(formula = formula, data = dataset_Xshap, index = c("GridID", "time"),
+          SDF = points_mesh, adaptive = F, p = 2, bigdata = F,
+          upperratio = 0.10, effect = "individual", model = "within", approach = "CV",
+          kernel = "bisquare",doParallel = T, cluster.number = 8, gradientIncrement = T,
+          GI.step = 0.0025, GI.upper = 0.061, GI.lower = 0.0025)
+saveRDS(GWPR.FEM.bandwidth.NDVI, '03_Results/GWPR.FEM.bandwidth.NDVI.rds')
+plot(GWPR.FEM.bandwidth.NDVI[,1], GWPR.FEM.bandwidth.NDVI[,2])
 
+formula <- ter_pressure_shap ~ ter_pressure
+
+GWPR.FEM.bandwidth.ter_pressure <- # this is about fixed bandwidth
+  bw.GWPR(formula = formula, data = dataset_Xshap, index = c("GridID", "time"),
+          SDF = points_mesh, adaptive = F, p = 2, bigdata = F,
+          upperratio = 0.10, effect = "individual", model = "within", approach = "CV",
+          kernel = "bisquare",doParallel = T, cluster.number = 8, gradientIncrement = T,
+          GI.step = 0.0025, GI.upper = 0.061, GI.lower = 0.0025)
+saveRDS(GWPR.FEM.bandwidth.ter_pressure_shap, '03_Results/GWPR.FEM.bandwidth.ter_pressure.rds')
+plot(GWPR.FEM.bandwidth.ter_pressure[,1], GWPR.FEM.bandwidth.ter_pressure[,2])
 
