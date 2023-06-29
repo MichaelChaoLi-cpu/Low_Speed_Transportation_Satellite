72ca2778f0f4ec0355e0411447f77d80aec0ad1b
MichaelChaoLi-cpu <chaoli0394@gmail.com>
2023-06-29 11:47:11 +0900

    C: add more figure to the repo

--- 00_RCode/13_AN_GwprXgbRev0618_v1.R
+++ 00_RCode/13_AN_GwprXgbRev0618_v1.R
@@ -42,7 +42,8 @@ formula <- tair_shap ~ tair
 reg_tair <- plm(formula, dataset_Xshap, index = c('GridID', 'time'), model = 'pooling')
 summary(reg_tair)
 
-moran.i.tair <- GWPR.moran.test(reg_tair, points_mesh, 0.0075)
+(moran.i.tair <- GWPR.moran.test(reg_tair, points_mesh, 0.0075))
+saveRDS(moran.i.tair, '12_Results0618/03.moran.i.tair.rds')
 
 GWPR.bw.tair <- # this is about fixed bandwidth
   bw.GWPR(formula = formula, data = dataset_Xshap, index = c("GridID", "time"),
@@ -68,6 +69,7 @@ reg_psurf <- plm(formula, dataset_Xshap, index = c('GridID', 'time'), model = 'p
 summary(reg_psurf)
 
 (moran.i.psurf <- GWPR.moran.test(reg_psurf, points_mesh, 0.0075))
+saveRDS(moran.i.tair, '12_Results0618/03.moran.i.psurf.rds')
 
 GWPR.result.psurf <- GWPR(formula = psurf_shap ~ psurf, data = dataset_Xshap, index = c("GridID", "time"),
                          SDF = points_mesh, bw = 0.0075, adaptive = F,
@@ -84,6 +86,7 @@ reg_qair <- plm(formula, dataset_Xshap, index = c('GridID', 'time'), model = 'po
 summary(reg_qair)
 
 (moran.i.qair <- GWPR.moran.test(reg_qair, points_mesh, 0.0075))
+saveRDS(moran.i.qair, '12_Results0618/03.moran.i.qair.rds')
 
 GWPR.result.qair <- GWPR(formula = qair_shap ~ qair, data = dataset_Xshap, index = c("GridID", "time"),
                           SDF = points_mesh, bw = 0.0075, adaptive = F,
@@ -100,6 +103,7 @@ reg_wind <- plm(formula, dataset_Xshap, index = c('GridID', 'time'), model = 'po
 summary(reg_wind)
 
 (moran.i.wind <- GWPR.moran.test(reg_wind, points_mesh, 0.0075))
+saveRDS(moran.i.wind, '12_Results0618/03.moran.i.wind.rds')
 
 GWPR.result.wind <- GWPR(formula = wind_shap ~ wind, data = dataset_Xshap, index = c("GridID", "time"),
                          SDF = points_mesh, bw = 0.0075, adaptive = F,
@@ -116,6 +120,7 @@ reg_rainf <- plm(formula, dataset_Xshap, index = c('GridID', 'time'), model = 'p
 summary(reg_rainf)
 
 (moran.i.rainf <- GWPR.moran.test(reg_rainf, points_mesh, 0.0075))
+saveRDS(moran.i.rainf, '12_Results0618/03.moran.i.rainf.rds')
 
 GWPR.result.rainf <- GWPR(formula = rainf_shap ~ rainf, data = dataset_Xshap, index = c("GridID", "time"),
                           SDF = points_mesh, bw = 0.0075, adaptive = F,
@@ -125,6 +130,61 @@ tm_shape(GWPR.result.rainf$SDF) +
   tm_dots(col = "rainf", pal = rev("RdYlBu"),midpoint = 0,
           style = 'cont')
 saveRDS(GWPR.result.rainf, '12_Results0618/02.GWPR.result.rainf.rds')
+
+### NTL
+formula <- NTL_shap ~ NTL
+reg_NTL <- plm(formula, dataset_Xshap, index = c('GridID', 'time'), model = 'pooling')
+summary(reg_NTL)
+
+(moran.i.NTL <- GWPR.moran.test(reg_NTL, points_mesh, 0.0075))
+saveRDS(moran.i.NTL, '12_Results0618/03.moran.i.NTL.rds')
+
+GWPR.result.NTL <- GWPR(formula = NTL_shap ~ NTL, data = dataset_Xshap, index = c("GridID", "time"),
+                          SDF = points_mesh, bw = 0.0075, adaptive = F,
+                          p = 2, effect = "individual", kernel = "bisquare", longlat = F, 
+                          model = "pooling")
+tm_shape(GWPR.result.NTL$SDF) +
+  tm_dots(col = "NTL", pal = rev("RdYlBu"),midpoint = 0,
+          style = 'cont')
+saveRDS(GWPR.result.NTL, '12_Results0618/02.GWPR.result.NTL.rds')
+
+### NDVI
+formula <- NDVI_shap ~ NDVI
+reg_NDVI <- plm(formula, dataset_Xshap, index = c('GridID', 'time'), model = 'pooling')
+summary(reg_NDVI)
+
+(moran.i.NDVI <- GWPR.moran.test(reg_NDVI, points_mesh, 0.0075))
+saveRDS(moran.i.NDVI, '12_Results0618/03.moran.i.NDVI.rds')
+
+GWPR.result.NDVI <- GWPR(formula = NDVI_shap ~ NDVI, data = dataset_Xshap, index = c("GridID", "time"),
+                        SDF = points_mesh, bw = 0.0075, adaptive = F,
+                        p = 2, effect = "individual", kernel = "bisquare", longlat = F, 
+                        model = "pooling")
+tm_shape(GWPR.result.NDVI$SDF) +
+  tm_dots(col = "NDVI", pal = rev("RdYlBu"),midpoint = 0,
+          style = 'cont')
+saveRDS(GWPR.result.NDVI, '12_Results0618/02.GWPR.result.NDVI.rds')
+
+### PBLH
+formula <- PBLH_shap ~ PBLH
+reg_PBLH <- plm(formula, dataset_Xshap, index = c('GridID', 'time'), model = 'pooling')
+summary(reg_PBLH)
+
+(moran.i.PBLH <- GWPR.moran.test(reg_NDVI, points_mesh, 0.0075))
+saveRDS(moran.i.PBLH, '12_Results0618/03.moran.i.PBLH.rds')
+
+GWPR.result.PBLH <- GWPR(formula = PBLH_shap ~ PBLH, data = dataset_Xshap, index = c("GridID", "time"),
+                         SDF = points_mesh, bw = 0.0075, adaptive = F,
+                         p = 2, effect = "individual", kernel = "bisquare", longlat = F, 
+                         model = "pooling")
+tm_shape(GWPR.result.PBLH$SDF) +
+  tm_dots(col = "PBLH", pal = rev("RdYlBu"),midpoint = 0,
+          style = 'cont')
+saveRDS(GWPR.result.PBLH, '12_Results0618/02.GWPR.result.PBLH.rds')
+
+
+
+
 
 
 
